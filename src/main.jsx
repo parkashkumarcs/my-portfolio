f5ffda8f5d87b087289092d7a2f0be0f573ce8fa
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2025-01-30 18:34:51 +0500

    Responsiveness changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
